2997c832f6ebc141e862751d0e69394600312a0f
Calum-W <cal7907@hotmail.co.uk>
2017-11-27 16:34:40 +0000

    Step 5: Adds a top_up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
 
   attr_reader :balance
-  
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(added)
+    @balance += added
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,10 @@ describe Oystercard do
       expect(oystercard.balance).to be_zero
     end
   end
+
+  describe "#top_up" do
+    it "adds a given amount to the balance" do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+  end
 end
