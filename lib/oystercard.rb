db772e2821afc9b75612c1205d5b53fa60ba9d9d
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-27 16:54:40 +0000

    Step 6: Adds test and method for default top up limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
   attr_reader :balance
+  DEFAULT_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(added)
+    fail "Exceeds balance limit (#{DEFAULT_LIMIT})" if @balance + added > DEFAULT_LIMIT
     @balance += added
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,7 @@ describe Oystercard do
   subject(:oystercard) { described_class.new }
 
   describe "#initialization" do
-    it "should initialize with a balance of 0" do
+    it "initializes with a balance of 0" do
       expect(oystercard.balance).to be_zero
     end
   end
@@ -12,6 +12,11 @@ describe Oystercard do
   describe "#top_up" do
     it "adds a given amount to the balance" do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it "raises an error when an added amount makes the balance exceed 90" do
+      oystercard.top_up(Oystercard::DEFAULT_LIMIT)
+      expect{ oystercard.top_up(1) }.to raise_error("Exceeds balance limit (#{Oystercard::DEFAULT_LIMIT})")
     end
   end
 end
