a6dad669ffab0497f8154f633a0e17b3935c436c
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-27 16:19:48 +0000

    Adds initialised balance and test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
 require "oystercard"
 describe Oystercard do
-  
+
+  subject(:oystercard) { described_class.new }
+
+  describe "#initialization" do
+    it "should initialize with a balance of 0" do
+      expect(oystercard.balance).to be_zero
+    end
+  end
 end
